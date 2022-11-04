3caa348e9fd04d13591848fe7c7f4bc1d939b583
Garima <66312580+Garimakanwaria@users.noreply.github.com>
2022-11-04 20:28:21 +0530

    Add files via upload

---
+++ bakery-order-app-master/src/App.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import Meals from "./components/Meals/Meals";
+import Cart from './components/Cart/Cart';
+import CartProvider from "./store/CartProvider";
+import Home from "./Home";
+import NotFound from "./NotFound";
+import About from "./About";
+import Gallery from "./components/Gallery";
+import bakeryImg from './assests/bake.jpeg';
+import classes from './components/Layout/Header.module.css';
+import HeaderCartButton from "./components/Layout/HeaderCartButton";
+import { Route, Switch } from "react-router-dom";
+import { Nav, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import Footer from "./Footer";
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <header className={classes.header}>
+        <LinkContainer to="/">
+          <Navbar.Brand className={classes.title}><h1>Anna's Bakery</h1></Navbar.Brand>
+        </LinkContainer>
+        <LinkContainer to="/about">
+          <Nav.Link className={classes.content}><h4>About Us</h4></Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/menu">
+          <Nav.Link className={classes.content}><h4>Menu</h4></Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/gallery">
+          <Nav.Link className={classes.content}><h4>Gallery</h4></Nav.Link>
+        </LinkContainer>
+        <HeaderCartButton onClick={showCartHandler} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={bakeryImg} alt='A table of breads' />
+      </div>
+
+
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/menu'>
+          <main>
+            <Meals />
+          </main>
+        </Route>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+        <Route exact path='/gallery'>
+          <Gallery />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+
+      <Footer />
+    </CartProvider>
+
+    
+  );
+}
+
+export default App;
---
+++ bakery-order-app-master/src/Footer.js
@@ -0,0 +1,8 @@
+
+const Footer = () => {
+    return (
+        <p style={{color: "white"}}>© 2021 Copyright: Yuelin Wen</p>
+    )
+}
+
+export default Footer;
